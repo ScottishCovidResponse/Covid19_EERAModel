bc6d398df0f56ce6ee0c918ba91c1ad66c00e15e
Kristian Zarebski <kristian.zarebski@ukaea.uk>
2020-05-14 15:32:21 +0100

    Converted functions to template functions
    SCRC-171 Refactor and test Utilities module

--- src/Utilities.h
+++ src/Utilities.h
@@ -5,25 +5,48 @@
 namespace EERAModel {
 namespace Utilities {
 
-double mean_calc_double(std::vector<double> v);
+template<typename T>
+double mean_calc( std::vector<T> v) {
+	T return_value = 0.0;
+	int n = v.size();
+	for(std::vector<double>::iterator j=v.begin();j!=v.end();++j)  return_value += *j;
+    return ( return_value / n);
+}
 
-void cumsum_calc(std::vector<double> v, std::vector<double>& r_val);
+template<typename T>
+void cumsum_calc(std::vector<T> v,std::vector<T>& r_val) {
+	int size_vec = v.size();
+	T tmp_sum=0.0;
+	for (int xx = 0; xx < size_vec; ++xx) {
+		tmp_sum += v[xx];
+		r_val.push_back(tmp_sum);
+	}
+}
 
-double max_calc(double a,double b);
+template<typename T>
+T max_calc(T a, T b) {
+	T rval;
+	if(a>=b) rval = a;
+	else rval = b;
+	return rval;
+}
 
-double sum_calc(std::vector<double> v);
+template<typename T>
+T sum_calc(std::vector<T> v) {
+    T rval = 0.0;
+	for(std::vector<double>::iterator j=v.begin();j!=v.end();++j)  rval += *j;
+	return rval;
+}
 
-double colsum_calc(std::vector<std::vector<double>> m, int colid);
+template<typename T>
+T colsum_calc(std::vector<std::vector<T>> m, int colid) {
+    T rval = 0;
+	for (unsigned int j = 0; j < m.size(); ++j) {
+		rval += m[j][colid];
+	}
+	return rval;
+}
 
-double mean_calc_int(std::vector<int> v);
-
-void cumsum_calc_int(std::vector<int> v, std::vector<int>& r_val);
-
-int max_calc_int(int a,int b);
-
-int sum_calc_int(std::vector<int> v);
-
-int colsum_calc_int(std::vector<std::vector<int>> m, int colid);
 
 } // namespace Utilities
 } // namespace EERAModel
