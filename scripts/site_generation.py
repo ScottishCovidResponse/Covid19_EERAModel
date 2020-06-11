3b9b216c738afca536b030002de7749682443a5c
Kristian Zarebski <kristian.zarebski@ukaea.uk>
2020-06-11 10:11:43 +0100

    Added Flawfinder

--- scripts/site_generation.py
+++ scripts/site_generation.py
@@ -3,9 +3,8 @@ import re
 
 class HTMLFileBuilder(object):
     def __init__(self, name="SCRC: COVID-19 EERA Model"):
-        self._data = {}
+        self._clang_tidy_data = {}
         self._name = name
-        self._input_file = None
         self._pages = {'cppcheck' : {'source' : 'cppcheck/index.html',
                              'title' : 'CPP Check',
                              'filename' : 'cppcheck.html'},
@@ -18,6 +17,45 @@ class HTMLFileBuilder(object):
                       'documentation' : {'source' : '../doc/eera_model_overview.html',
                                           'title' : 'Model Overview',
                                           'filename' : 'model_documentation.html'}}
+        self._flawfinder_data = {}
+
+    def parse_clangtidy(self, input_file):
+        with open(input_file) as f:
+            for line in f:
+                try:
+                    entry = re.findall('.+(src/\w+[.h|.cpp].+)', line)[0]
+                    entry  = entry.split(': ', 1)
+                    try:
+                        file_name, address = entry[0].split(':', 1)
+                    except ValueError:
+                        file_name = entry[0]
+                        address = ''
+                    library = re.findall('\[(.+)\]', entry[1])
+                    message = entry[1] if len(library) == 0 else entry[1].replace(' [{}]'.format(library[0]), '')
+                    if file_name not in self._clang_tidy_data:
+                        self._clang_tidy_data[file_name] = {}
+                    self._clang_tidy_data[file_name][address] = {'message' : message, 
+                                                 'library' : '' if len(library) == 0 else library[0]}
+                except IndexError:
+                    continue
+
+    def parse_flawfinder(self, input_file):
+      _lines_str = ''
+      with open(input_file) as f:
+        for line in f:
+          if re.findall('(^src/\w+[.h|.cpp].+)', line):
+            if _lines_str:
+              _key , _message = _lines_str.split(':  ', 1)
+              _library, _message = _message.split(':\n', 1)
+              _file, _address = _key.split(':')
+              if _file not in self._flawfinder_data:
+                self._flawfinder_data[_file] = {}
+              self._flawfinder_data[_file][_address] = {'message' : _message, 'library' : _library, 'file' : _file}
+            _lines_str = line
+          elif 'ANALYSIS' in line:
+            break
+          elif re.findall('  .+', line):
+            _lines_str += line
 
     def build_index(self):
       _index_str='''
@@ -62,6 +100,7 @@ class HTMLFileBuilder(object):
                 <a class="dropdown-item" href="site/code-coverage.html">Code Coverage</a>
                 <a class="dropdown-item" href="site/clang_tidy.html">Clang Tidy</a>
                 <a class="dropdown-item" href="site/cppcheck.html">CPP Check</a>
+                <a class="dropdown-item" href="site/flawfinder.html">Flawfinder</a>
               </div>
             </li>
           </ul>
@@ -85,6 +124,7 @@ class HTMLFileBuilder(object):
             <a href="site/code-coverage.html" class="btn btn-primary" role="button">Code Coverage Report</a>   
             <a href="site/clang_tidy.html" class="btn btn-primary" role="button">Clang Tidy Report</a>
             <a href="site/cppcheck.html" class="btn btn-primary" role="button">CPP Check</a>
+            <a href="site/flawfinder.html" class="btn btn-primary" role="button">Flawfinder</a>
          </div>
         <div class="container">
             <p></p>
@@ -119,27 +159,6 @@ class HTMLFileBuilder(object):
 
       with open('index.html', 'w') as f:
         f.write(_index_str)
-
-    def parse(self, input_file):
-        self._input_file = input_file
-        with open(self._input_file) as f:
-            for line in f:
-                try:
-                    entry = re.findall('.+(src/\w+[.h|.cpp].+)', line)[0]
-                    entry  = entry.split(': ', 1)
-                    try:
-                        file_name, address = entry[0].split(':', 1)
-                    except ValueError:
-                        file_name = entry[0]
-                        address = ''
-                    library = re.findall('\[(.+)\]', entry[1])
-                    message = entry[1] if len(library) == 0 else entry[1].replace(' [{}]'.format(library[0]), '')
-                    if file_name not in self._data:
-                        self._data[file_name] = {}
-                    self._data[file_name][address] = {'message' : message, 
-                                                 'library' : '' if len(library) == 0 else library[0]}
-                except IndexError:
-                    continue
 
     def build_wrapper_pages(self):
       
@@ -184,6 +203,7 @@ class HTMLFileBuilder(object):
             <a class="dropdown-item" href="{cov_addr}">Code Coverage</a>
             <a class="dropdown-item" href="{clang_addr}">Clang Tidy</a>
             <a class="dropdown-item" href="{cppc_addr}">CPP Check</a>
+            <a class="dropdown-item" href="{ff_addr}">Flawfinder</a>
           </div>
         </li>
       </ul>
@@ -211,8 +231,102 @@ class HTMLFileBuilder(object):
                                     doxy_addr=os.path.join('..', 'site', self._pages['doxygen']['filename']),
                                     cov_addr=os.path.join('..', 'site',self._pages['coverage']['filename']),
                                     clang_addr=os.path.join('..', 'site', self._pages['cppcheck']['filename']).replace('cppcheck','clang_tidy'),
+                                    ff_addr=os.path.join('..', 'site', self._pages['cppcheck']['filename']).replace('cppcheck','flawfinder'),
                                     cppc_addr=os.path.join('..', 'site', self._pages['cppcheck']['filename']),
                                     address_of_source=self._pages[page]['source'], site_name=self._name))
+
+    def build_flawfinder_html(self):
+      _body='''
+<!DOCTYPE HTML>
+<html lang="en-GB">
+    <head>
+    <meta charset="UTF-8">
+    <!-- Redirect -->
+    <!--<meta http-equiv="refresh" content="1;url=doxygen/html/index.html">-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    
+    <title>{site_name}</title>
+
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <!-- Brand -->
+      <a class="navbar-brand" href="../index.html">COVID-19 EERA Model</a>
+    
+      <!-- Links -->
+      <ul class="navbar-nav">
+        <!-- Dropdown -->
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+          Documentation
+        </a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="../site/model_documentation.html">Model Overview</a>
+          <a class="dropdown-item" href="../site/doxygen-docs.html">Doxygen</a>
+        </div>
+      </li>
+        <!-- Dropdown -->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="../index.html" id="navbardrop" data-toggle="dropdown">
+            Code Checks
+          </a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="../site/code-coverage.html">Code Coverage</a>
+            <a class="dropdown-item" href="../site/clang_tidy.html">Clang Tidy</a>
+            <a class="dropdown-item" href="../site/cppcheck.html">CPP Check</a>
+            <a class="dropdown-item" href="../site/flawfinder.html">Flawfinder</a>
+          </div>
+        </li>
+      </ul>
+    </nav> 
+        
+</head>
+    <body>
+    <div class="container">
+    <div class="jumbotron">
+        <h1>Flawfinder</h1>
+        <p>Listed below are the check results from Flawfinder.</p>
+    </div>
+    <p></p>
+    </div>
+    <div class="container">
+    <table class="table table-striped">
+    <thead>
+      <tr>
+        <th>Line</th>
+        <th>Library</th>
+        <th>Message</th>
+      </tr>
+    </thead>
+    {body}
+    </container>
+    </body>
+</html>
+      ''' 
+      _table_body = ''
+      for entry in self._flawfinder_data:
+        entry_str='''
+              <tr class="table-dark text-dark">
+              <td colspan="4" style="text-align:center"><strong>{file}</strong></td>
+              </tr>
+              '''.format(file=entry)
+        for address in sorted(self._flawfinder_data[entry].keys()):
+                entry_str+='''
+                <tr>
+                    <td>{address}</td>
+                    <td>{library}</td>
+                    <td>{message}</td>
+                </tr>
+                '''.format(file=entry, address=address,
+                            library=self._flawfinder_data[entry][address]['library'], message=self._flawfinder_data[entry][address]['message'])
+        _table_body += entry_str
+        
+        with open('site/flawfinder.html', 'w') as f:
+          f.write(_body.format(body=_table_body, site_name=self._name))
+
 
     def build_clang_html(self):
         _body='''
@@ -256,6 +370,7 @@ class HTMLFileBuilder(object):
             <a class="dropdown-item" href="../site/code-coverage.html">Code Coverage</a>
             <a class="dropdown-item" href="../site/clang_tidy.html">Clang Tidy</a>
             <a class="dropdown-item" href="../site/cppcheck.html">CPP Check</a>
+            <a class="dropdown-item" href="../site/flawfinder.html">Flawfinder</a>
           </div>
         </li>
       </ul>
@@ -290,14 +405,14 @@ class HTMLFileBuilder(object):
 
         _table_body = ''
 
-        for entry in self._data:
+        for entry in self._clang_tidy_data:
             entry_str='''
             <tr class="table-dark text-dark">
             <td colspan="4" style="text-align:center"><strong>{file}</strong></td>
             </tr>
             '''.format(file=entry)
-            for address in sorted(self._data[entry].keys()):
-                is_warning = 'warning' in self._data[entry][address]['message'].lower()
+            for address in sorted(self._clang_tidy_data[entry].keys()):
+                is_warning = 'warning' in self._clang_tidy_data[entry][address]['message'].lower()
                 button = _buttons['warning'] if is_warning else _buttons['info']
 
                 entry_str+='''
@@ -308,7 +423,7 @@ class HTMLFileBuilder(object):
                     <td>{message}</td>
                 </tr>
                 '''.format(button=button, file=entry, address=address,
-                            library=self._data[entry][address]['library'], message=self._data[entry][address]['message'])
+                            library=self._clang_tidy_data[entry][address]['library'], message=self._clang_tidy_data[entry][address]['message'])
             _table_body += entry_str
         
         with open('site/clang_tidy.html', 'w') as f:
@@ -318,6 +433,7 @@ class HTMLFileBuilder(object):
       self.build_index()
       self.build_wrapper_pages()
       self.build_clang_html()
+      self.build_flawfinder_html()
         
 
 if __name__ in "__main__":
@@ -325,10 +441,14 @@ if __name__ in "__main__":
 
     parser = argparse.ArgumentParser('ClangTidyParser')
 
-    parser.add_argument('input_file', help='Build log file')
+    parser.add_argument('build_log', help='Build log file')
+    parser.add_argument('flawfinder_log', help='Flaw Finder log file')
+
+    args = parser.parse_args()
 
     cl_parser = HTMLFileBuilder()
-    cl_parser.parse(parser.parse_args().input_file)
+    cl_parser.parse_clangtidy(args.build_log)
+    cl_parser.parse_flawfinder(args.flawfinder_log)
     cl_parser.Run()
     
     
