6d6b08a00b7f0131a6e275903f10eff42e92fe4a
Kristian Zarebski <kristian.zarebski@ukaea.uk>
2020-06-16 12:35:54 +0100

    Removed non-working getArg function and updated to struct style

--- src/CMDParse.cpp
+++ src/CMDParse.cpp
@@ -18,20 +18,20 @@ EERAModel::ArgumentParser::ArgumentParser(int argc, char** argv)
                                         "Model structure", 
                                         true, "", &allowedStructures);
 		TCLAP::ValueArg<std::string> modeArg("m", "mode", "Running mode, if unset use default", 
-                                        false, "", &allowedModes);
+                                        false, "inference", &allowedModes);
 		TCLAP::ValueArg<std::string> dataLocArg("l", "local", "Location of local data repository", 
                                         false, "", "string");
-        TCLAP::ValueArg<std::string> outputDirArg("d", "outdir", "Output directory of data files", false, std::string(ROOT_DIR)+"/outputs", "string");
+        TCLAP::ValueArg<std::string> outputDirArg("d", "outdir", "Output directory of data files", false, _args.output_dir, "string");
         cmd.add( modeArg );
         cmd.add( dataLocArg );
 		cmd.add( structureArg );
         cmd.add( outputDirArg );
 		cmd.parse( argc, argv);
-        _str_args["OutputDir"] = outputDirArg.getValue();
-		_str_args["Structure"] = structureArg.getValue();
-		_str_args["Mode"] = modeArg.getValue();
-        _bool_args["Local"] = dataLocArg.getValue().empty();
-        _str_args["Data Source"] = (_bool_args["Local"]) ? dataLocArg.getValue() : "scrc";
+        _args.output_dir = outputDirArg.getValue();
+		_args.stucture = (Utilities::toUpper(structureArg.getValue()) == "IRISH") ? ModelStructureId::IRISH : ModelStructureId::ORIGINAL;
+		_args.mode = (Utilities::toUpper(modeArg.getValue()) == "INFERENCE") ? ModelModeId::INFERENCE : ModelModeId::PREDICTION;
+        _args.isLocal = dataLocArg.getValue().empty();
+        _args.local_location = (_bool_args["Local"]) ? dataLocArg.getValue() : "scrc";
 	}
 	catch(const std::exception& e)
 	{
@@ -60,23 +60,7 @@ void EERAModel::ArgumentParser::logArguments(Utilities::logging_stream::Sptr log
 
 void EERAModel::ArgumentParser::AppendOptions(EERAModel::ModelInputParameters& input_params)
 {
-    if(Utilities::toUpper(_str_args["Structure"]) == "IRISH")
-    {
-        input_params.model_structure = ModelStructureId::IRISH;
-    }
+    input_params.model_structure = _args.stucture;
 
-    else
-    {
-        input_params.model_structure = ModelStructureId::ORIGINAL;
-    }
-
-    if(Utilities::toUpper(_str_args["Mode"]) == "INFERENCE")
-    {
-        input_params.run_type = ModelModeId::INFERENCE;
-    }
-
-    else
-    {
-        input_params.run_type = ModelModeId::PREDICTION;
-    }
+    input_params.run_type = _args.mode;
 }
