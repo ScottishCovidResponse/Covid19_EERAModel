020b7cc2cd1723f980af1f52718a1f155185cb55
Kristian Zarebski <kristian.zarebski@ukaea.uk>
2020-06-17 15:07:51 +0100

    Removed unneeded duplicate function

--- src/DataSourcing.cpp
+++ src/DataSourcing.cpp
@@ -4,47 +4,46 @@ namespace EERAModel
 {
 namespace DataSourcing
 {
-void DataSource::Setup()
-{
-    // Read prior particle parameters if run type is "Prediction"
-    if(_input_params.run_type == ModelModeId::PREDICTION)
+    void DataSource::Setup()
     {
-        _input_params.prior_param_list = _prior_params.prior_param_list;
-    }
-}
-
-void LocalSource::_extract_data()
-{  
-    setModelInputParameters(IO::ReadParametersFromFile(_data_files, getLogger()));
-
-    // Read prior particle parameters if run type is "Prediction"
-    if(getInputParameters().run_type == ModelModeId::PREDICTION)
-    {
-        setPriors(IO::ReadPriorParametersFromFile(_data_files, getLogger()));
+        // Read prior particle parameters if run type is "Prediction"
+        if(_input_params.run_type == ModelModeId::PREDICTION)
+        {
+            _input_params.prior_param_list = _prior_params.prior_param_list;
+        }
     }
 
-    setInputObservations(IO::ReadObservationsFromFiles(_data_files, getLogger()));
-}
+    void LocalSource::_extract_data()
+    {  
+        setModelInputParameters(IO::ReadParametersFromFile(_data_files, getLogger()));
 
-void RemoteSource::_extract_data()
-{
+        // Read prior particle parameters if run type is "Prediction"
+        if(getInputParameters().run_type == ModelModeId::PREDICTION)
+        {
+            setPriors(IO::ReadPriorParametersFromFile(_data_files, getLogger()));
+        }
 
-    setModelInputParameters(API::ReadParametersFromAPI(_api_info, getLogger()));
-
-    if(getInputParameters().run_type == ModelModeId::PREDICTION)
-    {
-        setPriors(API::ReadPriorParametersFromAPI(_api_info, getLogger()));
+        setInputObservations(IO::ReadObservationsFromFiles(_data_files, getLogger()));
     }
 
-    setInputObservations(API::ReadObservationsFromAPI(_api_info, getLogger()));
-    
-}
+    void RemoteSource::_extract_data()
+    {
 
-const DataSource getSource(SourceID source_id, Utilities::logging_stream::Sptr log,  std::string data_location)
-{
-    return (source_id == SourceID::LOCAL) ? DataSource(LocalSource(data_location, log)) : DataSource(RemoteSource(data_location, log));
-}
+        setModelInputParameters(API::ReadParametersFromAPI(_api_info, getLogger()));
 
-void PushOutputs() {}
+        if(getInputParameters().run_type == ModelModeId::PREDICTION)
+        {
+            setPriors(API::ReadPriorParametersFromAPI(_api_info, getLogger()));
+        }
+
+        setInputObservations(API::ReadObservationsFromAPI(_api_info, getLogger()));
+        
+    }
+
+    const DataSource getSource(SourceID source_id, Utilities::logging_stream::Sptr log,  std::string data_location)
+    {
+        return (source_id == SourceID::LOCAL) ? DataSource(LocalSource(data_location, log)) : DataSource(RemoteSource(data_location, log));
+    }
+
 };
 };
