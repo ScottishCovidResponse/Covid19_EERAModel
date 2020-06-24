c58f9bcfe37f3d3d6375628eaa883e3a6dea5ec2
peter-t-fox <64200987+peter-t-fox@users.noreply.github.com>
2020-06-24 13:13:16 +0100

    Cleaned up utilities implementation

--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -1,23 +1,22 @@
 #include "Utilities.h"
 #include <sys/stat.h>
 
-namespace EERAModel
-{
-namespace Utilities
-{
-    std::string toUpper(std::string str)
-    {
-        std::string _temp = str;
-        std::transform(_temp.begin(), _temp.end(), _temp.begin(), toupper);
+namespace EERAModel {
+namespace Utilities {
 
-        return _temp;
-    }
+std::string toUpper(std::string str)
+{
+    std::transform(str.begin(), str.end(), str.begin(), toupper);
 
-    bool directoryExists(const std::string directory)
-    {
-        struct stat buffer;
-        
-        return (stat(directory.c_str(), &buffer) == 0 && S_ISDIR(buffer.st_mode));
-    }
-};
-};
+    return str;
+}
+
+bool directoryExists(const std::string directory)
+{
+    struct stat buffer;
+    
+    return (stat(directory.c_str(), &buffer) == 0 && S_ISDIR(buffer.st_mode));
+}
+
+}
+}
