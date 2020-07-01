bf59edb73342e44139c14ae194e840c130cf3fec
peter-t-fox <64200987+peter-t-fox@users.noreply.github.com>
2020-07-01 10:54:49 +0100

    Fixed broken argument parser test
    Break occurred because mode switch is now mandatory

--- test/unit/ArgParserTest.cpp
+++ test/unit/ArgParserTest.cpp
@@ -6,35 +6,29 @@
 
 TEST(TestArgumentParser, TestModeSwitching)
 {
-
     char* _test_args_inf[] = {"exe", "-m", "inference", NULL};
     int argc_inf = sizeof(_test_args_inf)/sizeof(char*) - 1;
     char** _p_test_args_inf = _test_args_inf;
     char* _test_args_prd[] = {"exe", "-m", "prediction", NULL};
     int argc_prd = sizeof(_test_args_prd)/sizeof(char*) - 1;
     char** _p_test_args_prd = _test_args_prd;
-    char* _test_args_default[] = {"exe", NULL};
-    int argc_default = sizeof(_test_args_default)/sizeof(char*) - 1;
-    char** _p_test_default = _test_args_default;
 
     EERAModel::ArgumentParser parse_inf(argc_inf, _p_test_args_inf);
     EERAModel::ArgumentParser parse_prd(argc_prd, _p_test_args_prd);
-    EERAModel::ArgumentParser parse_default(argc_default, _p_test_default);
     
     EXPECT_EQ(parse_inf.getArgs().mode, EERAModel::ModelModeId::INFERENCE);
     EXPECT_EQ(parse_prd.getArgs().mode, EERAModel::ModelModeId::PREDICTION);
-    EXPECT_EQ(parse_default.getArgs().mode, EERAModel::ModelModeId(0));
 }
 
 TEST(TestArgumentParser, TestStructureSwitching)
 {
-    char* _test_args_inf[] = {"exe", "-s", "original", NULL};
+    char* _test_args_inf[] = {"exe", "-s", "original", "-m", "inference", NULL};
     int argc_inf = sizeof(_test_args_inf)/sizeof(char*) - 1;
     char** _p_test_args_inf = _test_args_inf;
-    char* _test_args_prd[] = {"exe", "-s", "irish", NULL};
+    char* _test_args_prd[] = {"exe", "-s", "irish", "-m", "inference", NULL};
     int argc_prd = sizeof(_test_args_prd)/sizeof(char*) - 1;
     char** _p_test_args_prd = _test_args_prd;
-    char* _test_args_default[] = {"exe", NULL};
+    char* _test_args_default[] = {"exe", "-m", "inference", NULL};
     int argc_default = sizeof(_test_args_default)/sizeof(char*) - 1;
     char** _p_test_default = _test_args_default;
 
@@ -49,7 +43,7 @@ TEST(TestArgumentParser, TestStructureSwitching)
 
 TEST(TestArgumentParser, TestExistingOutputDirectory)
 {
-    char* _test_args[] = {"exe", "-d", "test", NULL};
+    char* _test_args[] = {"exe", "-d", "test", "-m", "inference", NULL};
     int argc = sizeof(_test_args)/sizeof(char*) - 1;
     char** _p_test_args = _test_args;
 
@@ -60,7 +54,7 @@ TEST(TestArgumentParser, TestExistingOutputDirectory)
 
 TEST(TestArgumentParser, TestNonExistingOutputDirectory)
 {
-    char* _test_args[] = {"exe", "-d", "my_outputs", NULL};
+    char* _test_args[] = {"exe", "-d", "my_outputs", "-m", "inference", NULL};
     int argc = sizeof(_test_args)/sizeof(char*) - 1;
     char** _p_test_args = _test_args;
 
